ff795b92a7f016c2172a049e70d35a57fdd05e2e
javi-crep <75953944+Javi-crep@users.noreply.github.com>
2021-03-30 13:54:54 +0200

    Changes in Movie class

--- Movie.h
+++ Movie.h
@@ -1,16 +1,34 @@
-using namespace std;
-
+#include "Actors.h"
 class Movie{
     private:
         string title;
         string genre;
         string director;
         int year;
-        vector<string> actors;
-        
-        string str(256, '\0');
-       // char description[256];
+        vector<Actors> actors;
+        char description[256];
         int price;
 
     public:
+        //getter and setter for Title attribute
+        void setTitle(string str){
+            title = str;
+        }
+        string getTitle(){
+            return title;
+        }
+        //getter and setter for Genre attribute
+        void setGenre(string str){
+            genre = str;
+        }
+        string getGenre(){
+            return genre;
+        }
+        //getter and setter for director attribute
+        void setDirector(string str){
+            director = str;
+        }
+        string getDirector(){
+            return Director;
+        }
 }
