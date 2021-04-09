13737f593238bb4cf4e5674a6e6c33b7ed0c80df
olganedelcu <olganedelcuam@gmail.com>
2021-04-09 13:12:54 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include "Actors.h"
 
+main() {
+    int option;
+
+    cout << "List actors:\n ";
+    vector<Actors> actorsFromFile = readActorsFromFile();
+    for(int i = 0; i< actorsFromFile.size();i++){
+        cout << actorsFromFile[i].getName() << endl;
+        cout << actorsFromFile[i].getBirthdate() << endl;
+        cout << endl;
+
+    }
+}
